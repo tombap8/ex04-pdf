b8626cab1d9b9152d7eee3083bbf047a743d382e
Tom B. Parker <tombap8@hotmail.com>
2026-06-11 17:28:04 +0900

    Update last-main.py

--- last-main.py
+++ last-main.py
@@ -12,7 +12,8 @@ from langchain_classic.chains.combine_documents import create_stuff_documents_ch
 from langchain_core.prompts import (   ChatPromptTemplate )
 from langchain_core.callbacks import   BaseCallbackHandler
 
-st.title("📄 PDF File Reader [API KEY 입력 필수]")
+st.title("📄 PDF File Reader")
+st.text("📄[API KEY 입력 필수]")
 st.write("----------------")
 
 
